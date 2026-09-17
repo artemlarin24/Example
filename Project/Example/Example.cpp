97b49c45ad39fea407e215167142f3a61c4875dd
Artem <artemlarin931@gmail.com>
2026-09-17 18:51:25 +0300

    Добавлена реализация класса Triangle

--- Project/Example/Example.cpp
+++ Project/Example/Example.cpp
@@ -1,9 +1,37 @@
 ﻿#include <iostream>
+
+class Triangle {
+	double _side;
+	double _height;
+public:
+	Triangle(double side, double height) {
+		_side = side;
+		_height = height;
+	}
+	double calculate_S() const {
+		return 0.5 * _side * _height;
+	}
+	void set_side(double side) {
+		_side = side;
+	}
+	void set_height(double height) {
+		_height = height;
+	}
+	double get_side() const {
+		return _side;
+	}
+	double get_height() const {
+		return _height;
+	}
+};
+
 int main() {
-	double a, h, S;
-	std::cout << "Input side and height" << std::endl;
-	std::cin >> a >> h;
-	S = 0.5 * a * h;
-	std::cout << "Area: " << S << std::endl;
+	std::cout << "Input side and height of triangle:  " << std::endl;
+	double side, height;
+	std::cin >> side;
+	std::cin >> height;
+	Triangle T(side, height);
+	double S = T.calculate_S();
+	std::cout << S;
 	return 0;
 }
